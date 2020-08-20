e6f99c6adf57f7eab3e065e94cacac8baad79721
bruno pessoa <brunopessoa.melo@gmail.com>
2020-08-20 15:50:38 -0300

    criando templates header e footer

---
+++ loja-crud/inc/footer.php
@@ -0,0 +1,14 @@
+</main><!--/container-->
+
+<hr>
+
+    <footer class="container">
+        <p>&copy: 2018 - Bruno Pessoa Nunes De Melo</p>
+    </footer>
+
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="<?php echo BASEURL; ?>js/bootstrap.min.js"></script>
+    <script src="<?php echo BASEURL; ?>js/main.js"></script>
+
+    </body>
+</html>
