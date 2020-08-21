ef5d97767f10f7b68d20482911f74e589657307b
bruno pessoa <brunopessoa.melo@gmail.com>
2020-08-21 17:35:51 -0300

    aplicando a lógica do módulo e implementando a consulta do banco de dados.

--- loja-crud/inc/database.php
+++ loja-crud/inc/database.php
@@ -27,6 +27,40 @@ function close_database($conn){
 
 
     /**Pesquisa um Registro pelo ID em uma Tabela */
+
+    function find( $table = null, $id = null){
+        $database = open_database();
+        $found = null;
+
+        try {
+            if($id){
+                $sql = "SELECT*FROM". $table. "WHERE id =". $id;
+                $result = $database->query($sql);
+
+                if($result->num_rows > 0){
+                    $found = $result-> fetch_assoc(MYSQLI_ASSOC);
+
+                    /**Método alternativo 
+                     * $found = array();
+                     * 
+                     * while ($row = $result-> fetch_assoc()){
+                     * array_push($found, $row);
+                     * }
+                     */
+                }
+            }
+        } catch(Exception $e){
+            $_SESSION['message'] = $e->GetMessage();
+            $_SESSION['type'] = 'danger';
+        }
+
+        close_database($database);
+        return $found;
+    }
+
+    function find_all($table){
+        return find($table);
+    }
 }
 
 
